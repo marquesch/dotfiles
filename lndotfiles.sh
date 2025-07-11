23bd3bdcbc7163a5eae1d5c1a658366bc025bda9
Carlos Marques <carlos.marques@grupo-3c.com>
2025-07-11 15:25:03 -0300

    lndotfiles.sh

---
+++ lndotfiles.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+link_file() {
+    source_file=$1
+    destination_file=$2
+
+    if [ ! -f "$source_file" ]; then
+        echo "$source_file not found"
+        return 1
+    fi
+
+    ln -s -f "$source_file" "$destination_file"
+
+    if [ $? -ne 0 ]; then
+        echo "Error linking $source_file to $destination_file"
+        echo "$?"
+        return 1
+    fi
+
+    return 0
+}
+
+dotfiles_dir=$(pwd)
+
+files_to_link=(
+    ".zshrc"
+    ".vimrc"
+    ".gitconfig"
+    ".p10k.zsh"
+    ".wezterm.lua" 
+)
+
+for file_name in "${files_to_link[@]}"; do
+    source_file="$dotfiles_dir/$file_name"
+    destination_file="$HOME/$file_name"
+    
+    link_file "$source_file" "$destination_file"
+done
+
