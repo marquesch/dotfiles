86512363b283b14c3d0039893b9c44ac7dad1620
Carlos Marques <chmarques93@gmail.com>
2025-07-28 23:43:16 -0300

    use kickstart init

--- .config/nvim/lua/config/fzf-lua.lua
+++
@@ -1,9 +0,0 @@
-local fzf_lua = require("fzf-lua")
-
-fzf_lua.setup {
-  defaults = {
-    git_icons = false,
-  }
-}
-
-vim.keymap.set('n', '<leader>ss', fzf_lua.lsp_live_workspace_symbols, opts)
--- .config/nvim/lua/config/vim-go.lua
+++
@@ -1,6 +0,0 @@
-vim.g.go_def_mode = "gopls"
-vim.g.go_fmt_autosave = 1 -- Auto format on save
-vim.g.go_fmt_command = "goimports" -- Use goimports for formatting
-vim.g.go_referrers_mode = "gopls"
-vim.g.go_info_mode = "gopls"
-vim.g.go_snippet_engine = "luasnip" -- Integrate with LuaSnip for snippets
