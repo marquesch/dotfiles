26da2f3d6edc8b5bdb48870673f219c271c7bdc1
Carlos Marques <chmarques93@gmail.com>
2025-07-31 23:56:57 -0300

    add session-manager and alpha

--- .config/nvim/lua/plugins/vimtree.lua
+++ .config/nvim/lua/plugins/vimtree.lua
@@ -1,5 +1,6 @@
 return {
 	"nvim-tree/nvim-tree.lua",
+	lazy = true,
 	opts = {
 		sort = {
 			sorter = "case_sensitive",
@@ -9,6 +10,9 @@ return {
 		},
 		renderer = {
 			group_empty = true,
+		},
+		filters = {
+			dotfiles = true,
 		},
 	},
 	config = function(_, opts)
--- .wezterm.lua
+++ .wezterm.lua
@@ -2,69 +2,58 @@ local wezterm = require("wezterm")
 local config = {}
 
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 config = {
-  automatically_reload_config = true,
-  window_close_confirmation = "NeverPrompt",
-  adjust_window_size_when_changing_font_size = false,
-  window_decorations = "NONE",
-  check_for_updates = false,
-  use_fancy_tab_bar = false,
-  tab_bar_at_bottom = false,
-  font_size = 11,
-  font = wezterm.font("MesloLGS Nerd Font Mono"),
-  enable_tab_bar = false,
-  color_scheme = 'nord',
-  keys = {
-    { key = 'h', mods = 'SHIFT|ALT', action = wezterm.action.SplitPane { direction = 'Left'} },
-    { key = 'h', mods = 'SHIFT|CTRL', action = wezterm.action.ActivatePaneDirection("Left") },
-    { key = 'j', mods = 'SHIFT|ALT', action = wezterm.action.SplitPane { direction = 'Down'} },
-    { key = 'j', mods = 'SHIFT|CTRL', action = wezterm.action.ActivatePaneDirection("Down") },
-    { key = 'l', mods = 'SHIFT|ALT', action = wezterm.action.SplitPane { direction = 'Right'} },
-    { key = 'l', mods = 'SHIFT|CTRL', action = wezterm.action.ActivatePaneDirection("Right") },
-    { key = 'k', mods = 'SHIFT|ALT', action = wezterm.action.SplitPane { direction = 'Up'} },
-    { key = 'k', mods = 'SHIFT|CTRL', action = wezterm.action.ActivatePaneDirection("Up") }
-  },
-  window_padding = {
-    left = 3,
-    right = 3,
-    top = 0,
-    bottom = 0,
-  },
-  window_background_opacity = 0.95,
-  hyperlink_rules = {
-    {
-      regex = "\\((\\w+://\\S+)\\)",
-      format = "$1",
-      highlight = 1,
-    },
-    {
-      regex = "\\[(\\w+://\\S+)\\]",
-      format = "$1",
-      highlight = 1,
-    },
-    {
-      regex = "\\{(\\w+://\\S+)\\}",
-      format = "$1",
-      highlight = 1,
-    },
-    {
-      regex = "<(\\w+://\\S+)>",
-      format = "$1",
-      highlight = 1,
-    },
-    {
-      regex = "[^(]\\b(\\w+://\\S+[)/a-zA-Z0-9-]+)",
-      format = "$1",
-      highlight = 1,
-    },
-    {
-      regex = "\\b\\w+@[\\w-]+(\\.[\\w-]+)+\\b",
-      format = "mailto:$0",
-    },
-  },
+	automatically_reload_config = true,
+	window_close_confirmation = "NeverPrompt",
+	adjust_window_size_when_changing_font_size = false,
+	window_decorations = "NONE",
+	check_for_updates = false,
+	use_fancy_tab_bar = false,
+	tab_bar_at_bottom = false,
+	font_size = 11,
+	font = wezterm.font("MesloLGS Nerd Font Mono"),
+	enable_tab_bar = false,
+	color_scheme = "nord",
+	window_padding = {
+		left = 5,
+		right = 5,
+		top = 3,
+		bottom = 3,
+	},
+	window_background_opacity = 0.95,
+	hyperlink_rules = {
+		{
+			regex = "\\((\\w+://\\S+)\\)",
+			format = "$1",
+			highlight = 1,
+		},
+		{
+			regex = "\\[(\\w+://\\S+)\\]",
+			format = "$1",
+			highlight = 1,
+		},
+		{
+			regex = "\\{(\\w+://\\S+)\\}",
+			format = "$1",
+			highlight = 1,
+		},
+		{
+			regex = "<(\\w+://\\S+)>",
+			format = "$1",
+			highlight = 1,
+		},
+		{
+			regex = "[^(]\\b(\\w+://\\S+[)/a-zA-Z0-9-]+)",
+			format = "$1",
+			highlight = 1,
+		},
+		{
+			regex = "\\b\\w+@[\\w-]+(\\.[\\w-]+)+\\b",
+			format = "mailto:$0",
+		},
+	},
 }
 return config
-
